a30284c26a0d0b6a6336dbbe1a2a79f87f8bd689
harshjainsk <harshjainsk531@gmail.com>
2022-09-06 01:49:38 +0530

    modularised get_all_products and fetched quantity entity

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,17 +1,33 @@
 import mysql.connector
 
 
-cnx = mysql.connector.connect(user = 'root', password = 'root',
-                              host = '127.0.0.1',
-                              database = 'grocery_store'
-                              )
+def get_all_products():
+    cnx = mysql.connector.connect(user='root', password='root',
+                                  host='127.0.0.1',
+                                  database='grocery_store'
+                                  )
 
-cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-query = "SELECT * FROM grocery_store.products"
+    query = "select products.product_id, products.name, products.um_id, products.price_per_unit, uom.uom_name from " \
+            "products inner join uom on products.um_id=uom.uom_id "
 
-cursor.execute(query)
+    cursor.execute(query)
 
-for (product_id, name, uom_id, price_per_unit) in cursor:
-    print(product_id, name, uom_id, price_per_unit)
-cnx.close()
+    response = []
+
+    for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+        response.append({
+            'product_id': product_id,
+            'name': name,
+            'uom_id': uom_id,
+            'price_per_unit': price_per_unit,
+            'uom_name': uom_name
+        })
+
+    cnx.close()
+    return response
+
+
+if __name__ == '__main__':
+    print(get_all_products())
