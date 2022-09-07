f0a57cca3e23d61492ea9fa407d8f9a0c67f8d4f
harshjainsk <harshjainsk531@gmail.com>
2022-09-07 20:42:50 +0530

    fetching order_details and adding the entries to database

--- backend/order_dao.py
+++ backend/order_dao.py
@@ -6,13 +6,6 @@ from sql_connection import get_sql_connection
 def insert_order(connection, order):
     cursor = connection.cursor()
 
-    # order_query = "INSERT INTO order (customer_name, total, datetime) VALUES (%s, %s, %s)"
-
-    # order_query = ("INSERT INTO order "
-    #                "(customer_name, total, datetime) "
-    #                "VALUES (%s, %s, %s)")
-    # order_data = (order['customer_name'], order['grand_total'], datetime.now())
-
     order_query = ("INSERT INTO orders "
                    "(customer_name, grand_total, date_time) "
                    "VALUES (%s, %s, %s)")
@@ -21,6 +14,23 @@ def insert_order(connection, order):
     cursor.execute(order_query, order_data)
     order_id = cursor.lastrowid
 
+    # fetching order_details and adding the entries to database
+
+    order_details_query = ("INSERT INTO order_details "
+                           "(order_id, product_id, quantity, total_price) "
+                           "VALUES (%s, %s, %s, %s)"
+                           )
+
+    order_details_data = []
+    for order_detail_record in order['order_details']:
+        order_details_data.append([
+            order_id,
+            int(order_detail_record['product_id']),
+            float(order_detail_record['quantity']),
+            float(order_detail_record['total_price'])
+        ])
+
+    cursor.executemany(order_details_query, order_details_data)
     connection.commit()
 
     return order_id
@@ -29,9 +39,9 @@ def insert_order(connection, order):
 if __name__ == '__main__':
     connection = get_sql_connection()
     print(insert_order(connection, {
-        'customer_name': 'neha',
+        'customer_name': 'ironman',
         'datetime': datetime.now(),
-        'grand_total': 500,
+        'grand_total': 1000,
         'order_details': [
             {
                 'product_id': 2,
